e4164744f8d131a88ce10b9113f079f232509fec
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-10-02 12:06:07 -0400

    Updated from Brython Server: 10/2/2018 12:06:07 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -52,27 +52,10 @@ b = int(input("For multiples of what number shall we print 'Buzz'? "))
 a = (list(range(1,n+1)))
 
 for i in a:
-    if f>b and f%b ==0: 
-        if (i%f == 0) :
-            print("FizzBuzz")
-        elif (i%b == 0):
-            print("Buzz")
-        elif (i%(b) ==0 and i%(f) ==0) :
-            print("FizzBuzz")
-        else: print(i)
-    elif b>f and b%f ==0: 
-        if (i%b == 0) :
-            print("FizzBuzz")
-        elif (i%f == 0):
-            print("Buzz")
-        elif (i%(b) ==0 and i%(f) ==0) :
-            print("FizzBuzz")
-        else: print(i)
-    else: 
-        if (i%f == 0 and i%(f*b) != 0 ) :
+    if (i%f == 0 and i%(f*b) != 0 ) :
             print("Fizz")
-        elif (i%b == 0 and i%(f*b) != 0):
+    elif (i%b == 0 and i%(f*b) != 0):
             print("Buzz")
-        elif (i%(b) ==0 and i%(f) ==0) :
+    elif (i%(b) ==0 and i%(f) ==0) :
             print("FizzBuzz")
-        else: print(i)
+    else: print(i)
